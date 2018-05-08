6991d8e43f389c2adeb7d83f89d7ae9b2c941e2d
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-07 22:06:14 -0600

    Add data cleaner and mock data
    Will implement the data cleaner on the mock data in the component did mount for now, although it will change to a fetch call soon.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import mockData from '../../Data/Mock'
+import FilmsDataHandler from '../../Helpers/FilmsDataHandler'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      
+    }
+  }
+
   render() {
     return (
       <div className="App">
