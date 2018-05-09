ffc1fd9eb3c5c21d4ad6eabf380eecb99740781a
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-08 21:14:53 -0600

    Add return to Loading component
    whoops

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './Loading.css'
 
 const Loading = () => {
-  <div>
-    <img src="/images/chewy.gif" />
-    <h4>Loading...</h4>
-  </div>;
+  return(
+    <div>
+      <img src="/images/chewy.gif" />
+      <h4>Loading...</h4>
+    </div>;
+  )
 }
 
 export default Loading
