9df3d3dd0282931862b60f361eb9c0727c925f87
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-08 21:04:40 -0600

    Add Crawl style

--- src/Components/OpeningCrawl/OpeningCrawl.js
+++ src/Components/OpeningCrawl/OpeningCrawl.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import './OpeningCrawl.scss'
+import './OpeningCrawl.css'
 
 const OpeningCrawl = ({crawlInfo}) => {
   return (
-    <div>
-      <h4>{crawlInfo.crawl}</h4>
-      <p>{crawlInfo.title}</p>
-      <p>Release date: {crawlInfo.release_date}</p>
-    </div>
+    <section class="star-wars">
+      <div class="crawl">
+        <h4>{crawlInfo.crawl}</h4>
+        <div class="title">
+          <p>{crawlInfo.title}</p>
+          <p>Release date: {crawlInfo.release_date}</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
