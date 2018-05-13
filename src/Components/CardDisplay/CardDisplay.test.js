d5150d12413d94d9413b2f59e779630398a0ea36
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-13 12:51:31 -0600

    Add snapshot and peopleCard test for CardDisplay prop

---
+++ src/Components/CardDisplay/CardDisplay.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import CardDisplay from './CardDisplay';
+import PeopleCard from '../Cards/PeopleCard'
+
+describe('CardDisplay', () => {
+  let wrapper;
+  let mockProps = {
+    cards: [{ name: 'cam' }],
+    toggleFavorite: jest.fn(),
+    favorites: [{ name: 'cam' }]
+  }
+
+  beforeEach(() => {
+    wrapper = mount(<CardDisplay {...mockProps}/>);
+    mockProps = {
+      cards: [],
+      toggleFavorite: jest.fn(),
+      favorites: [{ name: 'cam' }]
+    };
+  });
+
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a peopleCard if given correct prop', () => {
+    wrapper = mount(<CardDisplay 
+      {...mockProps} 
+      cards={[{name: 'Cam', homeworld: 'Earth' }]}
+    />);
+    
+    const card = wrapper.find('div').find('div.card')
+    expect(card.hasClass('peopleCard'))
+  });
+
+  
+});
